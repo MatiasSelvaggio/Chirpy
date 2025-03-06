80701f1325d74927857f944f9468b6e5a627e7c2
Matias D. Selvaggio <matias6selvaggio@gmail.com>
2025-03-06 17:10:36 +0000

    count visit page, and reset metrics, adjust route app, admin, api

--- main.go
+++ main.go
@@ -5,28 +5,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
 
 	"github.com/joho/godotenv"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 
 	godotenv.Load(".env")
 
-	portString := os.Getenv("PORT")
-	if portString == "" {
-		log.Fatal("PORT is not found in the environment")
-	}
-	filePathRootString := os.Getenv("FILE_PATH_ROOT")
-	if portString == "" {
-		log.Fatal("FILE_PATH_ROOT is not found in the environment")
-	}
+	portString, filePathRootString, apiConfig := startApp()
 
 	serverMux := http.NewServeMux()
 
-	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRootString))))
+	serverMux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRootString)))))
 
-	serverMux.HandleFunc("/healthz", handlerHealthz)
+	serverMux.HandleFunc("GET /api/healthz", handlerHealthz)
+
+	serverMux.HandleFunc("GET /admin/metrics", apiConfig.handlerMetrics)
+	serverMux.HandleFunc("POST /admin/reset", apiConfig.handlerReset)
 
 	server := &http.Server{
 		Addr:    portString,
@@ -37,8 +38,39 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func handlerHealthz(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+func startApp() (string, string, apiConfig) {
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	filePathRootString := os.Getenv("FILE_PATH_ROOT")
+	if portString == "" {
+		log.Fatal("FILE_PATH_ROOT is not found in the environment")
+	}
+	return portString, filePathRootString, apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	file, err := os.ReadFile("metrics.html")
+	if err != nil {
+		log.Fatal("something went wrong opening metrics.html")
+		return
+	}
+
+	fileContent := string(file)
+	updatedContent := fmt.Sprintf(fileContent, cfg.fileserverHits.Load())
+
+	w.Write([]byte(updatedContent))
 }
